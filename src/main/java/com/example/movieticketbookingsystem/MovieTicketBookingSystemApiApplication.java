980fbefdddfd02c8573c75c56a5d3b982d289e44
vasanth <msdvasa7@gmail.com>
2025-04-17 16:59:42 +0530

    first commit|movie ticket booking system api

---
+++ src/main/java/com/example/movieticketbookingsystem/MovieTicketBookingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieticketbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketBookingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketBookingSystemApiApplication.class, args);
+	}
+
+}
