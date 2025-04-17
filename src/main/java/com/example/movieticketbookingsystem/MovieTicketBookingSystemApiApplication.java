d36242361ee1f5f66081e7e1d5e90d1dd9eed3b5
vasanth <msdvasa7@gmail.com>
2025-04-17 17:57:03 +0530

    configrated the project

--- src/main/java/com/example/movieticketbookingsystem/MovieTicketBookingSystemApiApplication.java
+++ src/main/java/com/example/movieticketbookingsystem/MovieTicketBookingSystemApiApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovieTicketBookingSystemApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieTicketBookingSystemApiApplication.class, args);
+
+		SpringApplication.run(MovieTicketBookingSystemApiApplication.class, args
+		);
+		System.out.println("hello world");
 	}
 
 }
