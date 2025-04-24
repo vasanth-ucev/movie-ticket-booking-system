019c18beabfb820b489d40870cce9322ed829a57
vasanth <msdvasa7@gmail.com>
2025-04-24 14:58:14 +0530

    validation is still working

--- src/main/java/com/example/movieticketbookingsystem/dto/UserRegistrationRequest.java
+++ src/main/java/com/example/movieticketbookingsystem/dto/UserRegistrationRequest.java
@@ -1,24 +1,22 @@
 package com.example.movieticketbookingsystem.dto;
 
 import com.example.movieticketbookingsystem.enums.Role;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
 public record UserRegistrationRequest(
-        @NotNull
+        @NotNull @Min(4) @Max(8)
         String username,
         @NotNull
         @Email(message = "Email should be validate")
         String email,
-        @NotNull
+        @NotNull @Min(value = 6,message = "password should have min length of 6")
         String password,
         @NotNull(message = "user role is required")
         Role role,
         @NotNull
-        @Pattern(regexp = "^[6-9]\\d{10}$", message = "Phone number must be exactly 10 digits and start with 6-9")
+        @Pattern(regexp = "^[6-9]\\d{9}$", message = "Phone number must be exactly 10 digits and start with 6-9")
         String phoneNumber,
         @NotNull
         LocalDate dateOfBirth
